5f5d806445ac19864857442ffa4a872cb8972bb9
DarioPazmino <35546398+DarioPazmino@users.noreply.github.com>
2024-08-28 10:04:36 -0500

    api, nodemon, routing

--- clase2/1.http.js
+++ clase2/1.http.js
@@ -1,6 +1,6 @@
 const http = require('node:http')
-
-console.log(process.env)
+const fs = require('node:fs')
+// console.log(process.env)
 
 const desiredPort = process.env.PORT ?? 1234
 
@@ -15,6 +15,17 @@ const processRequest = (req, res) => {
   if (req.url === '/') {
     res.statusCode = 200 // OK
     res.end('<h1>Bienvenido a mi página de inicio!</h1>')
+  } else if (req.url === '/imagen.jpg') {
+    fs.readFile('./sunflower.jpg', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>500 Interal Server Error</h1>')
+      } else {
+        res.statusCode = 200 // no es necesario ya que es el default
+        res.setHeader('Content-Type', 'image/jpg')
+        res.end(data)
+      }
+    })
   } else if (req.url === '/contacto') {
     res.statusCode = 200 // OK
     res.end('<h1>Contact</h1>')
