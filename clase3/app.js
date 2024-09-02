69933fc4f6edb78846eda5bc8d8f64afd257c1c0
DarioPazmino <35546398+DarioPazmino@users.noreply.github.com>
2024-09-02 15:32:52 -0500

    more endpoints, validation and cors

--- clase3/app.js
+++ clase3/app.js
@@ -1,13 +1,50 @@
 const express = require('express')
 const crypto = require('node:crypto')
+const cors = require('cors')
+
 const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.use(express.json())
+// si se usa cors() sin opciones autoriza todo (*)
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'https://movies.com',
+      'https://some-accepted-origin.dev'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
 app.disable('x-powered-by') // deshabilitar el header X-Powedered-By: Express
+
+/* 
+**  métodos normales: GET/HEAD/POST
+**  métodos complejos: PUT/PATCH/DELETE
+**  CORS PRE-Flight
+**  OPTIONS
+*/
 
 // Todos los recursos que sean MOVIES se identifica con /movies
 app.get('/movies', (req, res) => {
+  // res.header('Access-Control-Allow-Origin', '*') // o puede ser tambien la ruta directa ej: http://localhost:8080
+  // cuando la peticion es del mismo ORIGIN no pasa el origin en el header
+  // http://localhost:1234 --> http://localhost:1234
+  /* const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+  } */
   const { genre } = req.query
   if (genre) {
     const filteredMovies = movies.filter(
@@ -28,19 +65,17 @@ app.get('/movies/:id', (req, res) => { //path-to-regexp
 })
 
 app.post('/movies', (req, res) => {
-  const {
-    title, genre, year, director, duration, rate, poster
-  } = req.body
+  const result = validateMovie(req.body)
 
+  if (result.error) {
+    // 422 se puede usar pero 400 es más común
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  // en base de datos
   const newMovie = {
     id: crypto.randomUUID(), // uuid v4
-    title,
-    genre,
-    year,
-    director,
-    duration,
-    rate: rate ?? 0,
-    poster
+    ...result.data
   }
 
   // Esto no seria REST porque estamos guardando el estado de la aplicación en memoria
@@ -48,6 +83,57 @@ app.post('/movies', (req, res) => {
 
   res.status(201).json(newMovie) // devolver el recurso para actualizar la caché del cliente
 })
+
+app.delete('/movies/:id', (req, res) => {
+  // causa error CORS
+  /* const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+  } */
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) return res.status(404).json({ message: 'Movie not found' })
+
+    const updateMovie = {
+      ...movies[movieIndex],
+      ...result.data
+    }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+// no requerido si se usa cors()
+/* app.options('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+  }
+  res.sendStatus(200)
+}) */
 
 const PORT = process.env.PORT ?? 1234
 
